033a71953f26b47862517ddf1a17ebc08e751ca9
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-14 18:41:09 -0700

    created useReduce instead of useState to control information changing and re-rendering

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 // Context is to help set global state, and to pass down props to super nested components/children
 
 const BlogContext = React.createContext();
 
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length +1}`}];
+    default:
+      return state;
+  }
+}
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts([...blogPosts, { title: `#${blogPosts.length + 1}` }]);
+    dispatch({ type: 'add_blogpost'})
   };
 
-  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>{children}</BlogContext.Provider>
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
 };
 
 export default BlogContext;
