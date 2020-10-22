2c924716f0875a7fb3c31bdc9574ec3e3eb594ba
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-21 18:21:36 -0700

    added callback functionality with addBlogPost to reroute you back to indexScreen

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -15,7 +15,13 @@ const CreateScreen = ({ navigation }) => {
       <Text style={styles.label}>Enter Content</Text>
       <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
 
-      <Button onPress={() => addBlogPost(title, content)}title="Add Post"/>
+      <Button 
+      onPress={() => {
+        addBlogPost(title, content, () => {
+          navigation.navigate('Index');
+        })
+      }}
+      title="Add Post"/>
     </View>
   )
 }
