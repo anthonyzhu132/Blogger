623b0a78931608ed52001163571c16eb81d8c1bc
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-21 17:51:48 -0700

    added and finished showscreen for each blogpost

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
 
-const ShowScreen = () => {
+const ShowScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    blogPost => blogPost.id === navigation.getParam('id')
+  );
+
+  navigation.getParam('id')
   return (
     <View>
       <Text>
         ShowScreen
+        {blogPost.title}
       </Text>
     </View>
   );
